c7be9961c60172ddaf536fde7fb303d2320502a1
unknown <buragohain4@gmail.com>
2022-08-24 01:55:20 +0530

    Adding Abstract Method vs Overriding

---
+++ AbstractVsOverriding/AbstractVsOverriding/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's learn about Abstract Class vs Overriding. \n");
+            Class2 obj1 = new Class2();
+            obj1.Show();
+            Class4 obj2 = new Class4();
+            obj2.Test2();
+        }
+    }
+
+    //Override Class
+    class Class1
+    {
+        public virtual void Show()
+        {
+            Console.WriteLine("Parent Class Show method.");
+
+        }
+    }
+    class Class2 : Class1
+    {
+        public override void Show() //Optional Re-implementation //Overriding
+        {
+            Console.WriteLine("Re-implementation of Parent Class Show Method.");
+        }
+    }
+
+    //Abstract Class
+    abstract class Class3
+    {
+        public void Test1(int i, int b)//Parent Class Test1 method
+        {
+            Console.WriteLine("Parent Class Test1 method.");
+
+        }
+
+        public abstract void Test2(); //Parent Class Test2 method
+    }
+    class Class4 : Class3
+    {
+        public override void Test2() //Mendatory Implementation //Overriding
+        {
+            Console.WriteLine("Implementation of Parent Class Test2 Method.");
+        }
+    }
+
+}
